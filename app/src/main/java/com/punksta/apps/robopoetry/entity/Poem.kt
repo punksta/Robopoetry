8d8a6224bafc4e77e24b8c251f2c67b3e644da81
Stanislav Shakirov <punksta@protonmail.com>
2017-11-11 21:17:19 +0300

    Make model automatic parcelable

--- app/src/main/java/com/punksta/apps/robopoetry/entity/Poem.kt
+++ app/src/main/java/com/punksta/apps/robopoetry/entity/Poem.kt
@@ -1,41 +1,19 @@
 package com.punksta.apps.robopoetry.entity
 
-import android.os.Parcel
-import android.os.Parcelable
+import paperparcel.PaperParcel
+import paperparcel.PaperParcelable
 
 /**
  * Created by stanislav on 1/1/17.
  */
+@PaperParcel
 data class Poem(val id: String,
                 val name: String,
                 val сutText: String,
                 val year : String,
-                val source: String) : Parcelable, EntityItem {
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        with(dest) {
-            writeString(id)
-            writeString(name)
-            writeString(сutText)
-            writeString(year)
-            writeString(source)
-        }
-    }
-
-    constructor(id: Parcel) : this(id.readString(), id.readString(), id.readString(), id.readString(), id.readString())
-
-
-    override fun describeContents(): Int = 0
-
-
+                val source: String) : PaperParcelable, EntityItem {
     companion object {
-        @JvmField val CREATOR: Parcelable.Creator<Poem> = object : Parcelable.Creator<Poem> {
-            override fun createFromParcel(`in`: Parcel): Poem {
-                return Poem(`in`)
-            }
-
-            override fun newArray(size: Int): Array<Poem?> {
-                return arrayOfNulls(size)
-            }
-        }
+        @JvmField
+        val CREATOR = PaperParcelPoem.CREATOR
     }
 }
--- app/src/main/java/com/punksta/apps/robopoetry/screens/common/PlayerTask.kt
+++ app/src/main/java/com/punksta/apps/robopoetry/screens/common/PlayerTask.kt
@@ -1,26 +1,46 @@
 package com.punksta.apps.robopoetry.screens.common
 
+import android.os.Parcelable
 import com.punksta.apps.robopoetry.entity.Celebration
 import com.punksta.apps.robopoetry.entity.CelebrationItem
 import com.punksta.apps.robopoetry.entity.Poem
 import com.punksta.apps.robopoetry.entity.WriterInfo
+import paperparcel.PaperParcel
+import paperparcel.PaperParcelable
 
 /**
  * Created by stanislav on 3/8/17.
  */
-sealed class PlayerTask(val voice: String)
+sealed class PlayerTask(val voice: String) : Parcelable
 
-
+@PaperParcel
 class CelebrationTask(voice: String,
-                  val celebrationItem: CelebrationItem,
-                  val userName: String,
-                  val celebration: Celebration) : PlayerTask(voice)
+                      val celebrationItem: CelebrationItem,
+                      val userName: String,
+                      val celebration: Celebration) : PlayerTask(voice), PaperParcelable {
+    companion object {
+        @JvmField
+        val CREATOR = PaperParcelCelebrationTask.CREATOR
+    }
+}
 
+@PaperParcel
 class PoemTask(voice: String,
                val poem: Poem,
-               val writer: WriterInfo) : PlayerTask(voice)
+               val writer: WriterInfo) : PlayerTask(voice), PaperParcelable {
 
+    companion object {
+        @JvmField
+        val CREATOR = PaperParcelPoemTask.CREATOR
+    }
+}
 
+@PaperParcel
 class GreetingTask(voice: String,
                    val name: String,
-                   val greeting: String) : PlayerTask(voice)
+                   val greeting: String) : PlayerTask(voice), PaperParcelable {
+    companion object {
+        @JvmField
+        val CREATOR = PaperParcelGreetingTask.CREATOR
+    }
+}
